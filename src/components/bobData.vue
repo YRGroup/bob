c7a6558d7d5c8fe88c0af68cd8d9ade77e773625
nullcss <nullyl@163.com>
2018-10-30 16:43:48 +0800

    css

--- src/components/bobData.vue
+++ src/components/bobData.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="section4 container " id="section4">
     <el-row class="content row">
-      <el-col :md="6" :sm="12" class="item">
+      <el-col :md="8" :sm="12" class="item">
         <div class="icon-wrapper">
           <div class="bg"></div>
           <p class="text"><span>10</span><span class="sub"> 年</span></p>
@@ -9,7 +9,7 @@
         <p class="p1">行业经验</p>
         <p class="p2">Industry experience</p>
       </el-col>
-      <el-col :md="6" :sm="12" class="item">
+      <el-col :md="8" :sm="12" class="item">
         <div class="icon-wrapper">
           <div class="bg"></div>
           <p class="text"><span>88</span><span class="sub"> 家</span></p>
@@ -17,7 +17,7 @@
         <p class="p1">客户选择</p>
         <p class="p2">Customer choice</p>
       </el-col>
-      <el-col :md="6" :sm="12" class="item">
+      <el-col :md="8" :sm="12" class="item">
         <div class="icon-wrapper">
           <div class="bg"></div>
           <p class="text"><span>180</span><span class="sub"> 项</span></p>
@@ -25,14 +25,14 @@
         <p class="p1">服务标准</p>
         <p class="p2">service standard</p>
       </el-col>
-      <el-col :md="6" :sm="12" class="item">
+      <!-- <el-col :md="6" :sm="12" class="item">
         <div class="icon-wrapper">
           <div class="bg"></div>
           <p class="text"><span>100</span><span class="sub"> %</span></p>
         </div>
         <p class="p1">客户满意度</p>
         <p class="p2">customer satisfaction</p>
-      </el-col>
+      </el-col> -->
     </el-row>
   </section>
 </template>
@@ -70,6 +70,7 @@ export default {
 @import "../less/common.less";
 
 #section4 {
+  margin-bottom: 50px;
 
   // background-image: url('../images/section4_bg.jpg');
   // background-attachment: fixed;
@@ -78,6 +79,7 @@ export default {
   .content {
     margin: 0 auto;
     flex-wrap: wrap;
+    max-width: 1200px;
 
     .icon-wrapper {
       position: relative;
@@ -93,6 +95,8 @@ export default {
         background: url('../images/icon-2.png') no-repeat center center;
         transition: all  ease-in-out  .5s;
         // transform: rotateZ(180deg);
+        animation: rotate infinite linear 3s;
+        
       }
 
       .text {
--- src/components/bobService.vue
+++ src/components/bobService.vue
@@ -1,214 +1,226 @@
 <template>
   <section class="section1 section" id="section1">
-      <h5 class="section-title">
-        <span>服务范围 / <span class="en">our service</span> </span>
-      </h5>
-      <div class="container-fluid wrapper">
-        <el-row class="cards">
+    <h5 class="section-title">
+      <span>服务范围 / <span class="en">our service</span> </span>
+    </h5>
+    <div class="container-fluid wrapper">
+      <el-row class="cards">
 
-          <el-col :lg="4" :sm="12" :xs="24" class="card">
-            <div class="mask  flex flex-column">
-              <p class="p1">
-                <span>A</span>nalytice
-              </p>
-              <div class="p2">
-                <p>品牌研究</p>
-                <p>品牌研究</p>
-                <p>品牌研究</p>
-                <p>品牌研究</p>
-              </div>
-              <p class="p3">+</p>
+        <el-col :lg="4" :sm="12" :xs="24" class="card">
+          <div class="mask  flex flex-column">
+            <p class="p1">
+              <span>A</span>nalytice
+            </p>
+            <div class="p2">
+              <p>品牌研究</p>
+              <p>品牌定位</p>
+              <p>品牌核心价值</p>
+              <p>品牌愿景</p>
+              <p>品牌使命</p>
+              <p>品牌主张</p>
+              <p>品牌命名</p>
             </div>
-          </el-col>
-          <el-col :lg="4" :sm="12" :xs="24" class="card">
-            <div class="mask  flex flex-column">
-              <p class="p1">
-                <span>A</span>nalytice
-              </p>
-              <div class="p2">
-                <p>品牌研究</p>
-                <p>品牌研究</p>
-                <p>品牌研究</p>
-                <p>品牌研究</p>
-              </div>
-              <p class="p3">+</p>
+            <!-- <p class="p3">+</p> -->
+          </div>
+        </el-col>
+        <el-col :lg="4" :sm="12" :xs="24" class="card">
+          <div class="mask  flex flex-column">
+            <p class="p1">
+              <span>B</span>randing
+            </p>
+            <div class="p2">
+              <p>logo标志设计</p>
+              <p>VI基础部分设计</p>
+              <p>VI应用部分设计</p>
+              <p>宣传册设计</p>
+              <p>PPT模板</p>
             </div>
-          </el-col>
-          <el-col :lg="4" :sm="12" :xs="24" class="card">
-            <div class="mask  flex flex-column">
-              <p class="p1">
-                <span>A</span>nalytice
-              </p>
-              <div class="p2">
-                <p>品牌研究</p>
-                <p>品牌研究</p>
-                <p>品牌研究</p>
-                <p>品牌研究</p>
-              </div>
-              <p class="p3">+</p>
-            </div>
-          </el-col>
-          <el-col :lg="4" :sm="12" :xs="24" class="card">
-            <div class="mask  flex flex-column">
-              <p class="p1">
-                <span>A</span>nalytice
-              </p>
-              <div class="p2">
-                <p>品牌研究</p>
-                <p>品牌研究</p>
-                <p>品牌研究</p>
-                <p>品牌研究</p>
-              </div>
-              <p class="p3">+</p>
-            </div>
-          </el-col>
-          <el-col :lg="4" :sm="12" :xs="24" class="card">
-            <div class="mask  flex flex-column">
-              <p class="p1">
-                <span>A</span>nalytice
-              </p>
-              <div class="p2">
-                <p>品牌研究</p>
-                <p>品牌研究</p>
-                <p>品牌研究</p>
-                <p>品牌研究</p>
-              </div>
-              <p class="p3">+</p>
-            </div>
-          </el-col>
-          <el-col :lg="4" :sm="12" :xs="24" class="card">
-            <div class="mask  flex flex-column">
-              <p class="p1">
-                <span>A</span>nalytice
-              </p>
-              <div class="p2">
-                <p>品牌研究</p>
-                <p>品牌研究</p>
-                <p>品牌研究</p>
-                <p>品牌研究</p>
-              </div>
-              <p class="p3">+</p>
-            </div>
-          </el-col>
-        </el-row>
-      </div>
-    </section>
+            <!-- <p class="p3">+</p> -->
+          </div>
+        </el-col>
+        <el-col :lg="4" :sm="12" :xs="24" class="card">
+          <div class="mask  flex flex-column">
+            <p class="p1">
+            <span>C</span>ampaign
+          </p>
+          <div class="p2">
+            <p>传播调研</p>
+            <p>传播策略</p>
+            <p>传播战役方案</p>
+            <p>媒体管理服务</p>
+            <p>线下事件活动服务</p>
+            <p>线上事件营销活动</p>
+          </div>
+            <!-- <p class="p3">+</p> -->
+          </div>
+        </el-col>
+        <el-col :lg="4" :sm="12" :xs="24" class="card">
+          <div class="mask  flex flex-column">
+            <p class="p1">
+            <span>D</span><span style="font-size:24px">igital & web</span>
+          </p>
+          <div class="p2">
+            <p>网站页面设计</p>
+            <p>后台制作</p>
+            <p>小程序开发</p>
+          </div>
+            <!-- <p class="p3">+</p> -->
+          </div>
+        </el-col>
+        <el-col :lg="4" :sm="12" :xs="24" class="card">
+          <div class="mask  flex flex-column">
+            <p class="p1">
+            <span>E</span>ditorial
+          </p>
+          <div class="p2">
+            <p>企业宣传片</p>
+            <p>年度影视服务</p>
+            <p>年度图片服务</p>
+          </div>
+            <!-- <p class="p3">+</p> -->
+          </div>
+        </el-col>
+        <el-col :lg="4" :sm="12" :xs="24" class="card">
+          <div class="mask  flex flex-column">
+            <p class="p1">
+            <span>F</span>abrication
+          </p>
+          <div class="p2">
+            <p>画册印制</p>
+            <p>服装定制</p>
+            <p>周边产品定制</p>
+          </div>
+            <!-- <p class="p3">+</p> -->
+          </div>
+        </el-col>
+      </el-row>
+    </div>
+  </section>
 </template>
 
 <script>
-export default {
-  name: "bobHeader",
-  props: {
-    msg: String
-  },
-  data() {
-    return {
-      show: false
-    };
-  },
-  methods: {
-    toogleSideNav() {
-      this.show = !this.show;
+  export default {
+    name: "bobHeader",
+    props: {
+      msg: String
+    },
+    data() {
+      return {
+        show: false
+      };
+    },
+    methods: {
+      toogleSideNav() {
+        this.show = !this.show;
+      }
     }
-  }
-};
+  };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
-@import "../less/variable.less";
-@import "../less/mixin.less";
-@import "../less/common.less";
+  @import "../less/variable.less";
+  @import "../less/mixin.less";
+  @import "../less/common.less";
 
-#section1 {
-  padding-top: 68px;
-  padding-bottom: 80px;
-  .wrapper {
-    margin: 0 100px;
-  }
-  .section-title {
-    margin-bottom: 60px;
-  }
+  #section1 {
+    padding-top: 68px;
+    padding-bottom: 80px;
 
-  .cards {
-    .card {
-      cursor: pointer;
-      .background-cover();
-      -webkit-box-flex: 1;
-      -moz-box-flex: 1;
-      -ms-flex: 1;
-      flex: 1;
-      height: 360px;
-      -webkit-transition: all 0.3s;
-      -moz-transition: all 0.3s;
-      transition: all 0.3s;
-      text-align: center;
-      -ms-flex-pack: distribute;
-      position: relative;
-      // background-image: url('../images/serviceBg1.jpg');
-      .mask {
-        z-index: 2;
-        justify-content: space-around;
-        height: 100%;
-        transition: all 0.5s;
-      }
-      &:hover {
-        .mask {
-          background: rgba(229, 77, 4, 0.5);
-          color: #fff;
-          .p1 {
-            color: #fff;
-          }
-          .p2 {
-            color: #fff;
-          }
-        }
-      }
+    .wrapper {
+      margin: 0 100px;
+    }
 
-      &:nth-of-type(odd) {
-        background-color: #eeeeee;
-      }
+    .section-title {
+      margin-bottom: 60px;
+    }
 
-      &:nth-of-type(even) {
-        background-color: #f7f7f7;
-      }
-
-      .p1 {
-        font-size: 28px;
-        color: #333;
-        font-weight: bold;
-
-        span {
-          font-size: 64px;
-        }
-      }
-
-      .p2 {
-        font-size: 16px;
-        line-height: 25px;
-        color: #333;
+    .cards {
+      .card {
+        cursor: pointer;
+        .background-cover();
+        -webkit-box-flex: 1;
+        -moz-box-flex: 1;
+        -ms-flex: 1;
+        flex: 1;
+        height: 360px;
+        -webkit-transition: all 0.3s;
+        -moz-transition: all 0.3s;
         transition: all 0.3s;
-      }
+        text-align: center;
+        -ms-flex-pack: distribute;
+        position: relative;
 
-      .p4 {
-        display: none;
-      }
+        // background-image: url('../images/serviceBg1.jpg');
+        .mask {
+          z-index: 2;
+          justify-content: space-around;
+          height: 100%;
+          transition: all 0.5s;
+        }
 
-      .p3 {
-        font-weight: bold;
-        font-size: 20px;
+        &:hover {
+          .mask {
+            background: rgba(229, 77, 4, 0.5);
+            color: #fff;
+
+            .p1 {
+              color: #fff;
+            }
+
+            .p2 {
+              color: #fff;
+            }
+          }
+        }
+
+        &:nth-of-type(odd) {
+          background-color: #eeeeee;
+        }
+
+        &:nth-of-type(even) {
+          background-color: #f7f7f7;
+        }
+
+        .p1 {
+          font-size: 28px;
+          color: #333;
+          font-weight: bold;
+
+          span {
+            font-size: 64px;
+          }
+        }
+
+        .p2 {
+          font-size: 16px;
+          line-height: 25px;
+          color: #333;
+          transition: all 0.3s;
+          min-height: 180px;
+        }
+
+        .p4 {
+          display: none;
+        }
+
+        .p3 {
+          font-weight: bold;
+          font-size: 20px;
+        }
+      }
+    }
+
+    @media (max-width: 768px) {
+      .wrapper {
+        margin: auto;
+      }
+    }
+
+    @media (max-width: 1100px) {
+      .wrapper {
+        margin: auto;
       }
     }
   }
-  @media (max-width: 768px) {
-    .wrapper {
-      margin: auto;
-    }
-  }
-  @media (max-width: 1100px) {
-    .wrapper {
-      margin: auto;
-    }
-  }
-}
 </style>
