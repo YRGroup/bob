78d794f09e69a5e7dd272581a575b2f8edb6b2b9
nullcss <nullyl@163.com>
2018-11-01 09:56:14 +0800

    css

--- src/assets/info.js
+++ src/assets/info.js
@@ -1,23 +1,20 @@
 const info = {
   'a': {
-    poetry: `<p>Analytics</p><p>美丽的梦和美丽的诗一样,都是可遇而不可求的</p>
-    <p>Adolescence was brimming with </p>
-    <p>transitions, secretions and hopelessness. </p>
-    <p>My slow rite into adulthood is brimming with</p>
-    <p>transitions, secretions and hopefulness.</p>
-    <p>Nothing has really changed, yet everything is changing. </p>
-    <p>I’m changing.</p>
-    <p>Adolescence was brimming with </p>
-    <p>transitions, secretions and hopelessness. </p>`,
-    items: `<p>aitem美丽的梦和美丽的诗一样,都是可遇而不可求的</p>
-    <p>Adolescence was brimming with </p>
-    <p>transitions, secretions and hopelessness. </p>
-    <p>My slow rite into adulthood is brimming with</p>
-    <p>transitions, secretions and hopefulness.</p>
-    <p>Nothing has really changed, yet everything is changing. </p>
-    <p>I’m changing.</p>
-    <p>Adolescence was brimming with </p>
-    <p>transitions, secretions and hopelessness. </p>`
+    poetry: `<h3>Analytics</h3><p>美丽的梦和美丽的诗一样,都是可遇而不可求的</p>
+    <p>Thealchemistsintheirsearchforgold</p>
+<p>discoveredmanyotherthingsofgreatervalue.</p>
+<p>Hewhowouldsearchforpearlsmustdivebelow.</p>
+<p>Wheretofindperalsisdependentupon</p>
+<p>askingtherightquestions</p>
+<p>Thepriceoftrying</p>
+<p>islessthanthecostofidling.</p>`,
+    items: `<p>品牌研究</p>
+    <p>品牌定位</p>
+    <p>品牌核心价值</p>
+    <p>品牌愿景</p>
+    <p>品牌使命</p>
+    <p>品牌主张</p>
+    <p>品牌命名</p>`
   },
   'b': {
     poetry: `<p>Branding</p><p>美丽的梦和美丽的诗一样,都是可遇而不可求的</p>
