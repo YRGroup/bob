e1b04ed0ef64814c81acc4d404afdedc4248a6ba
xuchuan <825077792@qq.com>
2018-11-16 17:04:16 +0800

    css

--- src/components/bobArticle.vue
+++ src/components/bobArticle.vue
@@ -5,24 +5,24 @@
 </template>
 <script>
 export default {
-  data () {
-    return {}
+  data() {
+    return {};
   },
-  created () {},
-  mounted () {
-    let article = document.getElementById('acticle')
-    if (article.getElementsByTagName('img').length) {
-      article.getElementsByTagName('img')[0].style.display = 'none'
+  created() {},
+  mounted() {
+    let article = document.getElementById("acticle");
+    if (article.getElementsByTagName("img").length) {
+      article.getElementsByTagName("img")[0].style.display = "none";
     }
   }
-}
+};
 </script>
 
 <style lang="less">
 .case-article {
   background: #fff;
-  max-width: 1200px;
-  padding: 30px 50px;
+  max-width: 900px;
+  padding: 0.2rem 0.3rem;
   margin-left: auto;
   margin-right: auto;
   margin-top: 20px;
@@ -31,10 +31,11 @@ export default {
   text-align: left;
   line-height: 24px;
   img {
-    // max-width: 100%;
-    width: 100%;
+    max-width: 100%;
+    // width: 100%;
     height: auto;
-    vertical-align: bottom;
+    display: block;
+    margin: 0 auto;
     p {
       line-height: 28px;
     }
