f16dad7672fab62a4d92911f887c83d5da07beab
xuchuan <825077792@qq.com>
2018-11-21 11:42:39 +0800

    info

--- src/assets/info.js
+++ src/assets/info.js
@@ -3,14 +3,14 @@ const info = {
     name: 'Analytics',
     introduction: '越来越多的用户通过互联网来了解我们，通过互联网拉近与用户的距离，来更好的为用户提供产品服务',
     poetry: `<h2>Analytics</h2>
-    <p>不讨论的代价比瞎讨论的代价还要大。</p>
-    <p>The alchemists in their search for gold</p>
-    <p>discovered many other things of greater value.</p>
-    <p>He who would search for pearls must dive below.</p>
+    <p>The alchemists in their <span>search</span> for gold</p>
+    <p>discovered many other things <span>of greater value</span>.</p>
+    <p><span>He</span> who would search for pearls <span>must dive</span> below.</p>
     <p>Where to find perals is dependent upon</p>
-    <p>asking the right questions.</p>
+    <p><span>asking the right questions</span>.</p>
     <p>The price of trying </p>
-    <p>is less than the cost of idling.</p>`,
+    <p>is less than the <span>cost of idling</span>.</p>`,
+    poetryCn: '<p>不讨论的代价比瞎讨论的代价还要大。</p>',
     items: ['品牌核心价值',
       '品牌研究',
       '品牌定位',
@@ -24,17 +24,17 @@ const info = {
     name: 'Branding',
     introduction: '统一的视觉形象与规范可确保品牌的专业性和可识别性，增强品牌体验。',
     poetry: `<h2>Branding</h2>
-    <p>划掉多余的，美化剩下的。</p>
-    <p>A creative man is motivated by the desire to achieve, </p>
-    <p>not by the desire to beat others.</p>
-    <p>In order to make every detail perfect, </p>
-    <p>you need to limit the number of details to perfect.</p>
-    <p>For better or for worse, </p>
-    <p>your company is a reflection of </p>
-    <p>your thinking, your character, your values.</p>
+    <p>A <span>creative man</span> is motivated by the desire to <span>achieve</span>, </p>
+    <p>not by the desire to <span>beat others</span>.</p>
+    <p>In order to <span>make</span> every detail <span>perfect</span>, </p>
+    <p>you need to <span>limit</span> the number of details to perfect.</p>
+    <p>For <span>better</span> or for <span>worse</span>, </p>
+    <p>your company is a <span>reflection</span> of </p>
+    <p>your <span>thinking</span>, your <span>character</span>, your <span>values</span>.</p>
     <p>Therefore, </p>
-    <p>Your brand is what other people say about you,</p>
-    <p>when you’re not in the room.</p>`,
+    <p>Your <span>brand is</span> what other people say about <span>you</span>,</p>
+    <p>when you're not in the room.</p>`,
+    poetryCn: '<p>划掉多余的，美化剩下的。</p>',
     items: ['VI基础部分设计',
       'VI应用部分设计',
       'LOGO标志设计',
@@ -46,15 +46,15 @@ const info = {
     name: 'Campaign',
     introduction: '越来越多的用户通过互联网来了解我们，通过互联网拉近与用户的距离，来更好的为用户提供产品服务。',
     poetry: `<h2>Campaign</h2>
-    <p>如果你外婆不明白你在搞什么，你就没有在搞事情。</p>
-    <p>Any powerful idea is </p>
-    <p>absolutely fascinating </p>
-    <p>and absolutely useless </p>
-    <p>until you choose to use it.</p>
-    <p>No value is created by insights </p>
-    <p>if they are not acted upon. </p>
-    <p>You do not really achieve anything </p>
-    <p>unless you can explain it to your grandmother. </p>`,
+    <p>Any <span>powerful</span> idea is </p>
+    <p>absolutely <span>fascinating</span></p>
+    <p>and absolutely <span>useless</span></p>
+    <p>until you <span>choose</span> to <span>use</span> it.</p>
+    <p>No value is created by <span>insights</span> </p>
+    <p>if they are not <span>acted upon</span>. </p>
+    <p>You do not really <span>achieve</span> anything </p>
+    <p><span>unless</span> you can <span>explain</span> it to your <span>grandmother</span>. </p>`,
+    poetryCn: '<p>如果你外婆不明白你在搞什么，你就没有在搞事情。</p>',
     items: ['线下事件活动服务',
       '线上事件营销活动',
       '传播侃拼方案',
@@ -68,16 +68,16 @@ const info = {
     name: 'Digital & Web',
     introduction: '越来越多的用户通过互联网来了解我们，通过互联网拉近与用户的距离，来更好的为用户提供产品和服务。',
     poetry: `<h2>Digits & web</h2>
-    <p>互联网最起码的作用是让你难以回首没有它的日子。</p>
-    <p>Internet gives you an exceptionally effective way to </p>
-    <p>broadcast who you are</p>
-    <p>to your target market quickly and efficiently.</p>
+    <p><span>Internet</span> gives you an exceptionally effective <span>way</span> to </p>
+    <p><span>broadcast</span> who you are</p>
+    <p>to your target market <span>quickly</span> and <span>efficiently</span>.</p>
     <p>You should use internet </p>
-    <p>as a drunken man uses lamp posts </p>
-    <p>for support rather than for illumination.</p>
+    <p>as a drunken man uses <span>lamp posts</span></p>
+    <p>for <span>support</span> rather than for illumination.</p>
     <p>Once you know something</p>
-    <p>you find it hard to imagine </p>
+    <p>you find it hard to <span>imagine</span> </p>
     <p>what it was like not to know it.</p>`,
+    poetryCn: '<p>互联网最起码的作用是让你难以回首没有它的日子。</p>',
     items: ['微信平台解决方案',
       '服务器运营外包',
       '高端官网定制',
@@ -90,13 +90,13 @@ const info = {
     name: 'Editorial',
     introduction: '视频、图片形式可以将品牌信息及理念以更为直观的形式传递给目标客户。',
     poetry: `<h2>Editorial</h2>
-    <p>纪录一下自己，给关心你的。纪录一下他人，给你关心的。</p>
-    <p>If you don't give the market the story to talk about, </p>
+    <p>If you don't <span>give</span> the <span>market</span> the <span>story</span> to talk about, </p>
     <p>They will define your brand's story for you.</p>
     <p>In a world where you can be anything, be yourself. </p>
     <p>Be who you are and say what you feel, </p>
     <p>because those who mind don't matter </p>
     <p>and those who matter don't mind.</p>`,
+    poetryCn: '<p>纪录一下自己，给关心你的。纪录一下他人，给你关心的。</p>',
     items: ['年度摄影服务',
       '年度影视服务',
       '家庭影像服务',
@@ -108,14 +108,14 @@ const info = {
   'f': {
     name: 'Fabrication',
     introduction: '将设计制作成具体的实物，帮助您节省时间和精力，专注于更加重要的目标。',
-    poetry: `<h2> Fabrication </h2> 
-    <p> 如果品牌是首诗歌， 造出的产品就是诗朗诵。 </p> 
+    poetry: `<h2> Fabrication </h2>
     <p> A brand is a voice and a product is a souvenir. </p>
     <p> Your premium brand </p>
     <p> had better be delivering something special,</p>
     <p> or it’ s not going to get the business. </p>
     <p> When people use your brand name as a verb,</p>
     <p> that is remarkably good or bad. </p>`,
+    poetryCn: '<p> 如果品牌是首诗歌， 造出的产品就是诗朗诵。 </p>',
     items: ['画册印制',
       '服装定制',
       '周边产品定制'
