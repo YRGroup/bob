7efac10fb5eda87a6a208b9b26fb312b43f6a02c
nullcss <nullyl@163.com>
2018-11-30 09:32:51 +0800

    css

--- src/components/bobService.vue
+++ src/components/bobService.vue
@@ -8,9 +8,9 @@
     </h5>
     <div class="container-fluid service-wrapper">
       <el-row class="cards">
-        <el-col :lg="4" :sm="8" :xs="12" class="card wow fadeInDown" data-wow-delay=".5s" data-wow-duration="1s">
-          <div class="mask flex flex-column">
-            <router-link to="/service/a">
+        <router-link to="/service/a">
+          <el-col :lg="4" :sm="8" :xs="12" class="card wow fadeInDown" data-wow-delay=".5s" data-wow-duration="1s">
+            <div class="mask flex flex-column">
               <p class="p1">
                 <span>A</span>nalytics
               </p>
@@ -23,16 +23,16 @@
                 <p>品牌主张</p>
                 <p>品牌命名</p>
               </div>
-            </router-link>
-            <p class="serviceIcon">
-               <i class="iconfont">&#xe63b;</i>
-            </p>
-            <!-- <p class="p3">+</p> -->
-          </div>
-        </el-col>
-        <el-col :lg="4" :sm="8" :xs="12" class="card wow fadeInDown" data-wow-delay=".3s" data-wow-duration="1.2s">
-          <div class="mask flex flex-column">
-            <router-link to="/service/b">
+              <p class="serviceIcon">
+                <i class="iconfont">&#xe63b;</i>
+              </p>
+              <!-- <p class="p3">+</p> -->
+            </div>
+          </el-col>
+        </router-link>
+        <router-link to="/service/b">
+          <el-col :lg="4" :sm="8" :xs="12" class="card wow fadeInDown" data-wow-delay=".3s" data-wow-duration="1.2s">
+            <div class="mask flex flex-column">
               <p class="p1">
                 <span>B</span>randing
               </p>
@@ -44,15 +44,15 @@
                 <p>PPT模板</p>
               </div>
               <!-- <p class="p3">+</p> -->
-            </router-link>
-            <p class="serviceIcon">
-               <i class="iconfont">&#xe601;</i>
-            </p>
-          </div>
-        </el-col>
-        <el-col :lg="4" :sm="8" :xs="12" class="card wow fadeInDown" data-wow-delay="0.5s" data-wow-duration="1s">
-          <div class="mask flex flex-column">
-            <router-link to="/service/c">
+              <p class="serviceIcon">
+                <i class="iconfont">&#xe601;</i>
+              </p>
+            </div>
+          </el-col>
+        </router-link>
+        <router-link to="/service/c">
+          <el-col :lg="4" :sm="8" :xs="12" class="card wow fadeInDown" data-wow-delay="0.5s" data-wow-duration="1s">
+            <div class="mask flex flex-column">
               <p class="p1">
                 <span>C</span>ampaign
               </p>
@@ -65,15 +65,15 @@
                 <p>传播策略</p>
               </div>
               <!-- <p class="p3">+</p> -->
-            </router-link>
-            <p class="serviceIcon">
-               <i class="iconfont">&#xe600;</i>
-            </p>
-          </div>
-        </el-col>
-        <el-col :lg="4" :sm="8" :xs="12" class="card wow fadeInDown" data-wow-delay=".3s" data-wow-duration="1.2s">
-          <div class="mask flex flex-column">
-            <router-link to="/service/d">
+              <p class="serviceIcon">
+                <i class="iconfont">&#xe600;</i>
+              </p>
+            </div>
+          </el-col>
+        </router-link>
+        <router-link to="/service/d">
+          <el-col :lg="4" :sm="8" :xs="12" class="card wow fadeInDown" data-wow-delay=".3s" data-wow-duration="1.2s">
+            <div class="mask flex flex-column">
               <p class="p1">
                 <span>D</span>igital & Web
               </p>
@@ -85,16 +85,16 @@
                 <p>业务系统开发</p>
                 <p>H5应用开发</p>
               </div>
-            </router-link>
-            <p class="serviceIcon">
-               <i class="iconfont">&#xe639;</i>
-            </p>
-            <!-- <p class="p3">+</p> -->
-          </div>
-        </el-col>
-        <el-col :lg="4" :sm="8" :xs="12" class="card wow fadeInDown" data-wow-delay="0.5s" data-wow-duration="1s">
-          <div class="mask flex flex-column">
-            <router-link to="/service/e">
+              <p class="serviceIcon">
+                <i class="iconfont">&#xe639;</i>
+              </p>
+              <!-- <p class="p3">+</p> -->
+            </div>
+          </el-col>
+        </router-link>
+        <router-link to="/service/e">
+          <el-col :lg="4" :sm="8" :xs="12" class="card wow fadeInDown" data-wow-delay="0.5s" data-wow-duration="1s">
+            <div class="mask flex flex-column">
               <p class="p1">
                 <span>E</span>ditorial
               </p>
@@ -106,16 +106,16 @@
                 <p>人物传记</p>
                 <p>宣传片</p>
               </div>
-            </router-link>
-            <p class="serviceIcon">
-               <i class="iconfont">&#xe62c;</i>
-            </p>
-            <!-- <p class="p3">+</p> -->
-          </div>
-        </el-col>
-        <el-col :lg="4" :sm="8" :xs="12" class="card wow fadeInDown" data-wow-delay=".3s" data-wow-duration="1.2s">
-          <div class="mask flex flex-column">
-            <router-link to="/service/f">
+              <p class="serviceIcon">
+                <i class="iconfont">&#xe62c;</i>
+              </p>
+              <!-- <p class="p3">+</p> -->
+            </div>
+          </el-col>
+        </router-link>
+        <router-link to="/service/f">
+          <el-col :lg="4" :sm="8" :xs="12" class="card wow fadeInDown" data-wow-delay=".3s" data-wow-duration="1.2s">
+            <div class="mask flex flex-column">
               <p class="p1">
                 <span>F</span>abrication
               </p>
@@ -126,159 +126,171 @@
                 <p>文化周边产品</p>
                 <p>场地装饰产品</p>
               </div>
-            </router-link>
-            <p class="serviceIcon">
-               <i class="iconfont">&#xe6bc;</i>
-            </p>
-            <!-- <p class="p3">+</p> -->
-          </div>
-        </el-col>
+              <p class="serviceIcon">
+                <i class="iconfont">&#xe6bc;</i>
+              </p>
+              <!-- <p class="p3">+</p> -->
+            </div>
+          </el-col>
+        </router-link>
       </el-row>
     </div>
   </section>
 </template>
 
 <script>
-import { WOW } from 'wowjs'
+  import {
+    WOW
+  } from 'wowjs'
 
-export default {
-  name: "bobHeader",
-  props: {
-    msg: String
-  },
-  data() {
-    return {
-      show: false
-    };
-  },
-  methods: {
-    toogleSideNav() {
-      this.show = !this.show;
+  export default {
+    name: "bobHeader",
+    props: {
+      msg: String
+    },
+    data() {
+      return {
+        show: false
+      };
+    },
+    methods: {
+      toogleSideNav() {
+        this.show = !this.show;
+      }
+    },
+    mounted() {
+      new WOW().init()
     }
-  },
-  mounted () {
-    new WOW().init()
-  }
-};
+  };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
-@import "../less/variable.less";
-@import "../less/mixin.less";
-@import "../less/common.less";
+  @import "../less/variable.less";
+  @import "../less/mixin.less";
+  @import "../less/common.less";
 
-#section1 {
-  background: #f5f5f5;
-  padding: 0.5rem 0;
-  .service-wrapper {
-    margin: 0 100px;
-  }
+  #section1 {
+    background: #f5f5f5;
+    padding: 0.5rem 0;
 
-  .section-title {
-    margin-bottom: 0.5rem;
-  }
-
-  .cards {
-    .card {
-      cursor: pointer;
-      .background-cover();
-      -webkit-box-flex: 1;
-      -moz-box-flex: 1;
-      -ms-flex: 1;
-      flex: 1;
-      height: 320px;
-      -webkit-transition: all 0.3s;
-      -moz-transition: all 0.3s;
-      transition: all 0.3s;
-      text-align: center;
-      -ms-flex-pack: distribute;
-      position: relative;
-      margin-bottom: 0.2rem;
-      padding: 0 7px;
-      box-sizing: border-box;
-      .mask {
-        .background-cover();
-        position: relative;
-        z-index: 2;
-        justify-content: flex-start;
-        height: 100%;
-        transition: all 0.5s;
-        padding-top: @font-size-xxl;
-        box-sizing: border-box;
-        color: #333;
-        .serviceIcon{
-          position: absolute;
-          bottom: -16px;
-          right: 0;
-          color: #f5f5f5;
-          opacity: .8;
-          font-size: .7rem;
-          z-index: -1;
-        }
-      }
-      &:hover {
-        // background-image: url("../images/s1.png");
-        // background-color: rgba(214, 94, 15, 0.7);
-        margin-top: -10px; 
-        .mask {
-          box-shadow: 0 5px 20px rgba(0, 0, 0, 0.2);
-          a {
-            color: @color-theme;
-          }
-        }
-      }
-
-      &:nth-of-type(odd) {
-        .mask {
-          background-color: #fff;
-        }
-      }
-
-      &:nth-of-type(even) {
-        .mask {
-          background-color: #fff;
-        }
-      }
-
-      .p1 {
-        font-size: @font-size-xxl;
-        font-weight: bold;
-        span {
-          font-size: 0.4rem;
-        }
-      }
-      .p2 {
-        font-size: 15px;
-        line-height: 25px;
-        margin-top: @font-size-xl;
-        text-align: center;
-      }
-
-      .p4 {
-        display: none;
-      }
-
-      .p3 {
-        font-weight: bold;
-        font-size: @font-size-xl;
-      }
-    }
-  }
-  @media (max-width: 1200px) {
     .service-wrapper {
-      margin: auto;
+      margin: 0 100px;
     }
-  }
-  @media (max-width: 768px) {
-    .service-wrapper {
-      margin: auto;
+
+    .section-title {
+      margin-bottom: 0.5rem;
     }
+
     .cards {
       .card {
-        height: 280px;
+        cursor: pointer;
+        .background-cover();
+        -webkit-box-flex: 1;
+        -moz-box-flex: 1;
+        -ms-flex: 1;
+        flex: 1;
+        height: 320px;
+        -webkit-transition: all 0.3s;
+        -moz-transition: all 0.3s;
+        transition: all 0.3s;
+        text-align: center;
+        -ms-flex-pack: distribute;
+        position: relative;
+        margin-bottom: 0.2rem;
+        padding: 0 7px;
+        box-sizing: border-box;
+        .mask {
+          .background-cover();
+          position: relative;
+          z-index: 2;
+          justify-content: flex-start;
+          height: 100%;
+          transition: all 0.5s;
+          padding-top: @font-size-xxl;
+          box-sizing: border-box;
+          color: #333;
+
+          .serviceIcon {
+            position: absolute;
+            bottom: -12px;
+            right: 0;
+            color: #f5f5f5;
+            opacity: .8;
+            font-size: .7rem;
+            z-index: -1;
+          }
+        }
+
+        &:hover {
+          // background-image: url("../images/s1.png");
+          // background-color: rgba(214, 94, 15, 0.7);
+          margin-top: -10px;
+
+          .mask {
+            box-shadow: 0 5px 20px rgba(0, 0, 0, 0.2);
+
+            a {
+              color: @color-theme;
+            }
+          }
+        }
+
+        &:nth-of-type(odd) {
+          .mask {
+            background-color: #fff;
+          }
+        }
+
+        &:nth-of-type(even) {
+          .mask {
+            background-color: #fff;
+          }
+        }
+
+        .p1 {
+          font-size: @font-size-xxl;
+          font-weight: bold;
+
+          span {
+            font-size: 0.4rem;
+          }
+        }
+
+        .p2 {
+          font-size: 15px;
+          line-height: 25px;
+          margin-top: @font-size-xl;
+          text-align: center;
+        }
+
+        .p4 {
+          display: none;
+        }
+
+        .p3 {
+          font-weight: bold;
+          font-size: @font-size-xl;
+        }
+      }
+    }
+
+    @media (max-width: 1200px) {
+      .service-wrapper {
+        margin: auto;
+      }
+    }
+
+    @media (max-width: 768px) {
+      .service-wrapper {
+        margin: auto;
+      }
+
+      .cards {
+        .card {
+          height: 280px;
+        }
       }
     }
   }
-}
 </style>
