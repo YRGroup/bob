e2ad03db282d6cc5be7f8018584cb927f653e919
xuchuan <825077792@qq.com>
2018-12-04 14:13:20 +0800

    nbd

---
+++ src/assets/nbd.js
@@ -0,0 +1,110 @@
+const nbd = [{
+  name: 'A',
+  types: [{
+    name: '资金',
+    icon: require('@/images/navBtn.png')
+  }, {
+    name: '市场',
+    icon: require('@/images/navBtn.png')
+  }, {
+    name: '产品',
+    icon: require('@/images/navBtn.png')
+  }]
+},{
+  name: 'B',
+  types: [{
+    name: '资金',
+    icon: require('@/images/navBtn.png')
+  }, {
+    name: '市场',
+    icon: require('@/images/navBtn.png')
+  }, {
+    name: '产品',
+    icon: require('@/images/navBtn.png')
+  }]
+},{
+  name: 'C',
+  types: [{
+    name: '资金',
+    icon: require('@/images/navBtn.png')
+  }, {
+    name: '市场',
+    icon: require('@/images/navBtn.png')
+  }, {
+    name: '产品',
+    icon: require('@/images/navBtn.png')
+  }]
+},{
+  name: 'D',
+  types: [{
+    name: '资金',
+    icon: require('@/images/navBtn.png')
+  }, {
+    name: '市场',
+    icon: require('@/images/navBtn.png')
+  }, {
+    name: '产品',
+    icon: require('@/images/navBtn.png')
+  }]
+},{
+  name: 'E',
+  types: [{
+    name: '资金',
+    icon: require('@/images/navBtn.png')
+  }, {
+    name: '市场',
+    icon: require('@/images/navBtn.png')
+  }, {
+    name: '产品',
+    icon: require('@/images/navBtn.png')
+  }]
+},{
+  name: 'F',
+  types: [{
+    name: '资金',
+    icon: require('@/images/navBtn.png')
+  }, {
+    name: '市场',
+    icon: require('@/images/navBtn.png')
+  }, {
+    name: '产品',
+    icon: require('@/images/navBtn.png')
+  }]
+},{
+  name: 'L',
+  types: [{
+    name: '资金',
+    icon: require('@/images/navBtn.png')
+  }, {
+    name: '市场',
+    icon: require('@/images/navBtn.png')
+  }, {
+    name: '产品',
+    icon: require('@/images/navBtn.png')
+  }]
+},{
+  name: 'S',
+  types: [{
+    name: '资金',
+    icon: require('@/images/navBtn.png')
+  }, {
+    name: '市场',
+    icon: require('@/images/navBtn.png')
+  }, {
+    name: '产品',
+    icon: require('@/images/navBtn.png')
+  }]
+},{
+  name: 'R',
+  types: [{
+    name: '资金',
+    icon: require('@/images/navBtn.png')
+  }, {
+    name: '市场',
+    icon: require('@/images/navBtn.png')
+  }, {
+    name: '产品',
+    icon: require('@/images/navBtn.png')
+  }]
+}]
+export default nbd
